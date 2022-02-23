5dcda4b812cc50e0e243459122d0ddfcea1d1e4f
Beck Yuldashev <yuldashev.bek@gmail.com>
2022-02-23 12:55:54 +0300

    Added header and home section

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,39 @@
 
 
 /*=============== SHOW MENU ===============*/
+const getElement = (selector) => {
+  const element = document.querySelector(selector);
 
+  if (element) return element;
+  
+  throw Error(
+    `Please, check your class names, there is no ${selector} class`
+  );
+};
+
+const navMenu = getElement('.nav__menu');
+const navToggle = getElement('#nav-toggle');
+const navClose = getElement('#nav-close');
+
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.add('show-menu');
+});
+
+navClose.addEventListener('click', () => {
+  navMenu.classList.remove('show-menu');
+});
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  const navMenu = getElement('.nav__menu');
+
+  navMenu.classList.remove('show-menu');
+}
+
+navLinks.forEach(elem => elem.addEventListener('click', linkAction));
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
